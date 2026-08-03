085a721d885e128d1f5c3924f49f7617cb7e1b3b
deepakr9 <deepakrtlk@gamil.com>
2026-08-03 12:13:32 +0530

    First program

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,6 @@ public class WelcomeToJava {
         System.out.println("Ialready know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using java.");
         System.out.println("Now i know how to use git  and github.");
+        System.out.println("my name is Sneha.");
     }
 }
