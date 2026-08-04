47f2839646d714b69bcf5fd9ed7376f3d62b0d44
deepakr9 <deepakrtlk@gamil.com>
2026-08-04 13:25:46 +0530

    PlacementEligible

---
+++ track/M02/T01/ST01/P01/PlacementEligible.java
@@ -0,0 +1,18 @@
+package track.M02.T01.ST01.P01;
+
+public class PlacementEligible {
+    public static void main(String[] args) {
+
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+
+        System.out.println("Placement Eligible: " + placementEligible);
+    }
+    
+}
