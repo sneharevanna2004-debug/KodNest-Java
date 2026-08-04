cac00aeb7c49fb215502e1e4c31ec242ba85a9b3
deepakr9 <deepakrtlk@gamil.com>
2026-08-04 11:35:19 +0530

    Practicee

--- track/M02/T01/ST01/P01/Practice.java
+++ track/M02/T01/ST01/P01/Practice.java
@@ -2,8 +2,11 @@ package track.M02.T01.ST01.P01;
 public class Practice{
     public static void main(String[] args){ 
          System.out.println("Monday");
-         System.out.println("TuesdayWedensday");
-         System.out.println("FridaySatursday");
+         System.out.print("Tuesday");
+         System.out.println("Wednesday");
+         System.out.println("Thursday");
+         System.out.print("Friday");
+         System.out.println("Satursday");
          System.out.println("Sunday"); 
     }
     
