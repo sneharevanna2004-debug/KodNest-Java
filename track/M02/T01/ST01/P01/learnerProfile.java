5ca1ac810811804d7d84c4d78e64996f4b8d6e78
deepakr9 <deepakrtlk@gamil.com>
2026-08-04 10:35:06 +0530

    learnerProfile

---
+++ track/M02/T01/ST01/P01/learnerProfile.java
@@ -0,0 +1,15 @@
+package track.M02.T01.ST01.P01;
+
+public class learnerProfile {
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
+
+ 
+    
+
