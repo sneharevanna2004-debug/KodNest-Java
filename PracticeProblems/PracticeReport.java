8b056f5eb4d15aa1312154a6360b7fc6f5bac912
deepakr9 <deepakrtlk@gamil.com>
2026-08-04 12:06:31 +0530

    PracticeReport

---
+++ PracticeProblems/PracticeReport.java
@@ -0,0 +1,17 @@
+package PracticeProblems;
+
+public class PracticeReport {
+     public static void main(String[] args) {
+
+        double solvedProblems = 47.0;
+        int practiceDays = 86;
+        double exactPercentage = 9.4;
+
+        System.out.println("Report Value: " + solvedProblems);
+        System.out.println("Whole Percentage: " + practiceDays);
+        System.out.println("Average Per Day: " + exactPercentage);
+        System.out.println("Daily Target Reached: " + true);
+    }
+}
+    
+
