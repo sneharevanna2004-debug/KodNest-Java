a1f659022a44addc12b8166d10fa9c22d7a8172a
deepakr9 <deepakrtlk@gamil.com>
2026-08-05 12:19:25 +0530

    increment

---
+++ track/M02/T01/ST01/P01/increment.java
@@ -0,0 +1,13 @@
+package track.M02.T01.ST01.P01;
+
+public class increment {
+    public static void main(String[] args) {
+       // int a =10;
+        //int res = a++ + ++a + a++ + a++ + ++a + a++;
+        //System.out.println(res);
+        //System.out.println(a);
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
