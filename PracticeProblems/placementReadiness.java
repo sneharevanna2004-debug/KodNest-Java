aed106a9b38462d17971b691d5cab4eac11e5656
deepakr9 <deepakrtlk@gamil.com>
2026-08-05 11:19:07 +0530

    Dayswitch

---
+++ PracticeProblems/placementReadiness.java
@@ -0,0 +1,20 @@
+public class placementReadiness{
+    public static void main(String[] args) {
+
+        int marks = 72;
+        int attendance = 80;
+        boolean activeBacklog = false;
+        boolean projectCompleted = true;
+        int mockInterviewScore = 65;
+
+        if (marks < 60 || attendance < 75 || activeBacklog) {
+            System.out.println("Improve Academic Eligibility");
+        } else if (!projectCompleted) {
+            System.out.println("Complete the project");
+        } else if (mockInterviewScore < 70) {
+            System.out.println("Improve Interview Skills");
+        } else {
+            System.out.println("Placement Ready");
+        }
+    }
+}
---
+++ track/M02/T01/ST01/P01/Dayswitch.java
@@ -0,0 +1,28 @@
+package track.M02.T01.ST01.P01;
+
+public class Dayswitch {
+    public static void main(String[] args) {
+
+        int day = 6;
+
+        switch (day) {
+
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Learning Day");
+                break;
+
+            case 6:
+            case 7:
+                System.out.println("Revision Day");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+    
+}
