2a8ed5280f07d210656f0ffe92e23f59255e9f37
deepakr9 <deepakrtlk@gamil.com>
2026-08-06 12:48:40 +0530

    Initial commit

---
+++ track/M02/T01/ST01/P01/Nestedcontinue.java
@@ -0,0 +1,12 @@
+public class Nestedcontinue {
+    public static void main(String[] args){
+        for (int i=1;i<=3;i++){
+            for (int j=1;j<=4;j++){
+                if(j==2){
+                    continue;
+                }
+                System.out.println("in"+i + "j : " +j);
+            }
+        }
+    }
+}
