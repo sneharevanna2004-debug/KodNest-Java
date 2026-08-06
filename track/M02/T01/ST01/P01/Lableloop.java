731d7a4bd5a1eed7aea98bd058514fa9f90b7b29
deepakr9 <deepakrtlk@gamil.com>
2026-08-06 13:00:57 +0530

    Initial commit

---
+++ track/M02/T01/ST01/P01/Lableloop.java
@@ -0,0 +1,13 @@
+public class Lableloop {
+    public static void main(String[] arga){
+        outer: for(int i=1;i<=3;i++){
+            inner : for(int j=1;j<=3;j++){
+                if(i==2){
+                    break outer;
+                }
+                System.out.println("i : "+i + "j : " +j);
+            }
+        }
+    }
+    
+}
