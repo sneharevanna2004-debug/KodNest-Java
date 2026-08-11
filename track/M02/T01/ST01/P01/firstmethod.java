d553f60af56e07296186174d1c31629825cc63a7
deepakr9 <deepakrtlk@gamil.com>
2026-08-11 17:04:33 +0530

    Initial commit

---
+++ track/M02/T01/ST01/P01/firstmethod.java
@@ -0,0 +1,14 @@
+
+    class Robot {
+    void speak() {
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+public class firstmethod {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.speak();
+    }
+}
+
