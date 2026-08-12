543616997c49e6b0a87a7506e1313af427b36f58
deepakr9 <deepakrtlk@gamil.com>
2026-08-12 11:20:55 +0530

    Initial commit

---
+++ PracticeProblems/foodOrder.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class OrderCalculator {
+    int calculateSubtotal(int price, int quantity) {
+        return price * quantity;
+    }
+
+    int calculateFinalTotal(int subtotal, int deliveryCharge) {
+        return subtotal + deliveryCharge;
+    }
+}
+
+public class foodOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int price = scanner.nextInt();
+        int quantity = scanner.nextInt();
+        int deliveryCharge = scanner.nextInt();
+
+        OrderCalculator calculator = new OrderCalculator();
+
+        int subtotal = calculator.calculateSubtotal(price, quantity);
+        int finalTotal = calculator.calculateFinalTotal(subtotal, deliveryCharge);
+
+        System.out.println("Subtotal: " + subtotal);
+        System.out.println("Final Total: " + finalTotal);
+    }
+}
