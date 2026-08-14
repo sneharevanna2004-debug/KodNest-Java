826aa76bb485436deb0f15a723779976d62966bb
deepakr9 <deepakrtlk@gamil.com>
2026-08-14 11:52:24 +0530

    Initial commit

---
+++ track/M02/T01/ST01/P01/Student1.java
@@ -0,0 +1,15 @@
+public class Student1 {
+    String name;
+    int age;
+    double height;
+    Student1(String name, int age, double height){
+        this.name = name;
+        this.age = age;
+        this.height= height;
+    }
+    void display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
---
+++ track/M02/T01/ST01/P01/studentApp.java
@@ -0,0 +1,6 @@
+public class studentApp {
+    public static void main (String [] agrs){
+        Student1 s1 = new Student1("Raja", 18 , 5.5);
+        s1.display();
+    }
+}
