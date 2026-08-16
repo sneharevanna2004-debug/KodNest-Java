0742d38724e0af4f56839baabff0ff27b77121e5
deepakr9 <deepakrtlk@gamil.com>
2026-08-16 15:43:54 +0530

    Initial commit

---
+++ PracticeProblems/cabBooking.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+class CabBooking {
+    String passengerName;
+    String cabType;
+    int distance;
+    int fare;
+
+    // Quick booking constructor
+    CabBooking(String passengerName) {
+        this.passengerName = passengerName;
+        this.cabType = "Mini";
+        this.distance = 5;
+        this.fare = 75;
+    }
+
+    // Custom booking constructor
+    CabBooking(String passengerName, String cabType, int distance) {
+        this.passengerName = passengerName;
+        this.cabType = cabType;
+        this.distance = distance;
+        this.fare = distance * 15;
+    }
+
+    void displayBooking() {
+        System.out.println("Passenger: " + passengerName);
+        System.out.println("Cab Type: " + cabType);
+        System.out.println("Distance: " + distance + " km");
+        System.out.println("Fare: Rs. " + fare);
+    }
+}
+
+public class cabBooking {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        String passengerName = scanner.next();
+
+        CabBooking booking;
+
+        if (choice == 1) {
+            booking = new CabBooking(passengerName);
+        } else {
+            String cabType = scanner.next();
+            int distance = scanner.nextInt();
+            booking = new CabBooking(passengerName, cabType, distance);
+        }
+
+        booking.displayBooking();
+    }
+}
