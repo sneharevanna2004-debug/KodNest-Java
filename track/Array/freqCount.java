a1c8261dbd8ae6c12e3790560bf9b78719518346
deepakr9 <deepakrtlk@gamil.com>
2026-08-18 12:33:40 +0530

    Initial commit

---
+++ track/Array/freqCount.java
@@ -0,0 +1,19 @@
+package Array;
+class Freq {
+    int count =0;
+    void count(int arr[], int key){
+        for(int i=0;i<arr.length-1;i++){
+         if(arr[i] ==key){
+            count++;
+         }
+        }
+        System.out.println(key + " :" +count);
+    }
+}
+public class freqCount {
+    public static void main (String[] args){
+        int arr[] = {10,20,10,30,10,30};
+        Freq fs = new Freq();
+        fs.count(arr,30);
+    }
+}
